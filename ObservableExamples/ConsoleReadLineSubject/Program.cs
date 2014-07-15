586a514d54e13fc353c6ba9d17fe6c1327818cad
ghotiphud <yale.cason@gmail.com>
2014-07-15 11:57:30 -0400

    init

---
+++ ObservableExamples/ConsoleReadLineSubject/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+
+namespace ConsoleReadLine
+{
+    class Program
+    {
+        static Subject<int> subject = new Subject<int>();
+
+        static void Main(string[] args)
+        {
+            // a
+            var a = 0;
+            subject
+                .Where(i => i % 2 == 0)
+                .Subscribe(
+                    onNext: i => { a += i; Console.WriteLine("a: {0}", a); },
+                    onCompleted: () => Console.WriteLine("a: {0}", a));
+
+            // b
+            subject
+                .Where(i => i > 5)
+                .Sum()
+                .Subscribe(b => Console.WriteLine("b: {0}", b));
+
+            // c
+            subject
+                .Where(i => i < 10)
+                .Sum()
+                .Subscribe(c => Console.WriteLine("c: {0}", c));
+
+            { // Read every line from the console and publish to the subject.
+                var line = "";
+                var input = 0;
+                while (true)
+                {
+                    line = Console.ReadLine();
+                    if (int.TryParse(line, out input))
+                    {
+                        subject.OnNext(input);
+                    }
+
+                    if (line == "exit")
+                    {
+                        subject.OnCompleted();
+                        break;
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObservableExamples/SimpleObservable/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleObservable
+{
+    class Program
+    {
+        static Stopwatch stopwatch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 5, 6, 7, 9, 3, 1, 56, 9, 3, 12 };
+            stopwatch.Start();
+            Enumerable_Blocking(GetHeavyEnumerable(list));
+            Console.WriteLine("time: {0}", stopwatch.Elapsed);
+
+            stopwatch.Restart();
+            Console.WriteLine("time restarted");
+            Observable_NonBlocking(GetHeavyEnumerable(list));
+            Console.WriteLine("time: {0}", stopwatch.Elapsed);
+
+            Console.ReadKey();
+        }
+
+        static void Enumerable_Blocking(IEnumerable<int> list)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            foreach (var i in list)
+            {
+                if (i % 2 == 0) a += i;
+                if (i > 5) b += i;
+                if (i < 10) c += i;
+            }
+
+            Console.WriteLine("a: {0}", a);
+            Console.WriteLine("b: {0}", b);
+            Console.WriteLine("c: {0}", c);
+        }
+
+        static async void Observable_NonBlocking(IEnumerable<int> list)
+        {
+            var observable = list.ToObservable(Scheduler.Default);
+
+            // a
+            // subscribe overload with onNext and onComplete.
+            var a = 0;
+            observable
+                .Where(i => i % 2 == 0)
+                .Subscribe(
+                    onNext: i => a += i,
+                    onCompleted: () => Console.WriteLine("a: {0}", a));
+
+            // b
+            // Sum keeps a running total until the onComplete call.
+            observable
+                .Where(i => i > 5)
+                .Sum()
+                .Subscribe(b => Console.WriteLine("b: {0}", b));
+
+            // c
+            // show how observables can be awaited.
+            var obc = observable
+                .Where(i => i < 10)
+                .Sum();
+
+            Console.WriteLine("c: {0}", await obc.LastAsync());
+            Console.WriteLine("time: {0}", stopwatch.Elapsed);
+        }
+
+        static IEnumerable<int> GetHeavyEnumerable(IEnumerable<int> list)
+        {
+            foreach (var i in list)
+            {
+                Thread.Sleep(200);
+                yield return i;
+            }
+        }
+    }
+}
